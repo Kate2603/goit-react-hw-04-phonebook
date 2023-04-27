a8d442e60b0b1317b29cd8a7b083b30766ff853c
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2023-04-27 13:24:19 +0300

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { GlobalStyle } from 'components/Globalstyle.js';
+import { Layout, Header, MainHeader } from './Layout.js';
+import { ContactsForm } from 'components/ContactsForm/ContactsForm.jsx';
+import { ContactList } from 'components/ContactList/ContactList.jsx';
+import { Filter } from 'components/Filter/Filter.jsx';
+import useLocaleStorage from './useLocalStorage.js';
+
+const KEY = 'contacts';
+
+const App = () => {
+  const [contacts, setContacts] = useLocaleStorage(KEY, []);
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    contacts.filter(contact => contact.name === newContact.name).length
+      ? alert(`${newContact.name}: is already in contacts`)
+      : setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const changeFilter = event => {
+    return setFilter(event.currentTarget.value);
+  };
+
+  const getFilterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filterContacts = getFilterContacts();
+
+  return (
+    <Layout>
+      <GlobalStyle />
+      <MainHeader>Phonebook</MainHeader>
+      <ContactsForm onSubmit={addContact} />
+      <Header>Contacts</Header>
+      <Filter onChange={changeFilter} value={filter}></Filter>
+      {contacts.length > 0 && (
+        <ContactList
+          contacts={filterContacts}
+          onDeleteContact={deleteContact}
+        ></ContactList>
+      )}
+    </Layout>
+  );
+};
+
+export { App };
